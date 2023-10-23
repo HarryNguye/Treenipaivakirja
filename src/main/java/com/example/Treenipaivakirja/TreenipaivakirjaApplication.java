fbf7963734230bce6bce4089b4bfd261b50d50a7
HarryNguye <harrynguyen100603@gmail.com>
2023-10-23 20:35:32 +0300

    first commit

---
+++ src/main/java/com/example/Treenipaivakirja/TreenipaivakirjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Treenipaivakirja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreenipaivakirjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreenipaivakirjaApplication.class, args);
+	}
+
+}
